088286e6fb2a617f503cdae4f88ba7358a75dd3f
Ben Avrahami <sample@notreal.fake>
2019-06-23 13:39:27 +0300

    initial

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.0 - 2019-06-23
+## Added
+* Initial release
---
+++ tests/sandbox.py
@@ -0,0 +1,31 @@
+from io import StringIO
+
+from time_me import *
+
+strings = [str(i) for i in range(1000)]
+coach = TimeLimitCoach(0.1, sanity_argsets={
+    (): ''.join(strings)
+})
+
+
+@coach.measure
+def add():
+    ret = ''
+    for i in strings:
+        ret += i
+    return ret
+
+
+@coach.measure
+def join():
+    return ''.join(strings)
+
+
+@coach.measure
+def buffer():
+    ret = StringIO()
+    ret.writelines(strings)
+    return ret.getvalue()
+
+
+coach.compare().bar()
